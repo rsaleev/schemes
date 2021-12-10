0b566bab146a6c2b24e8cb66dd087feb922ccd33
rsaleev <rs.aleev@gmail.com>
2021-12-10 11:58:45 +0300

    процедура без вычислений

--- src/service/routes/validation.py
+++ src/service/routes/validation.py
@@ -3,26 +3,38 @@ from typing import Tuple, List
 import concurrent.futures
 
 from fastapi.routing import APIRouter
-from fastapi import Query
+from fastapi import Query, HTTPException, status
+from starlette.responses import JSONResponse
 
 from src.service.models import scheme
 
-from src.core.schemes import WorkbookSchemes
+from src.core.schemes import WorkbookSchemes, Workbook
 
 router = APIRouter(tags=['scheme', 'validation'])
 
 
-
-def background_validation(schemes:List[dict], data:Tuple[str, ...]):
-    with concurrent.futures.ThreadPoolExecutor(max_workers=len(schemes)) as executor:
-        results = {executor.submit(s.verify,*headers):s for s in schemes}
-        for future in concurrent.futures.as_completed(results):
-
-@router.get('/validate/{source}')
+@router.get('/validate/{source}', response_model=scheme.SchemeResponse, status_code=status.HTTP_200_OK)
 async def validate_schema(source:scheme.SchemeSource, headers:Tuple[str, ...]=Query(...)):
+    schemes = []
     if source == scheme.SchemeSource.documents:
-        schemes = await WorkbookSchemes.load()
-    
+        schemes.extend(WorkbookSchemes.load())
+    elif source == scheme.SchemeSource.database:
+        raise HTTPException(status.HTTP_501_NOT_IMPLEMENTED)
+    # проверка загрузки схем
+    if not schemes:
+        return scheme.SchemeResponse(error='Схема не найдена')
+    # проверка входных данных на совпадение с моделью заголовка документа
+    columns_verifying_results = [s.verify_columns(headers) for s in schemes]
+    result = next(cfr for cfr in columns_verifying_results if cfr[0])
+    if not result:
+        return scheme.SchemeResponse(error='Схема не найдена')
+    # проверка на совпадение по схеме, значение отличное от None
+    try:
+        output = result.validate_columns(*result)
+    except Exception as e:
+        return scheme.SchemeResponse(error=str(e))
+    else:
+        return scheme.SchemeResponse(data=output)
             
 
 
