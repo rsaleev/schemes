d559131d1ee585cc778c2d924b67f43890911430
rsaleev <rs.aleev@gmail.com>
2021-12-10 11:56:16 +0300

    добавлены операции с данными

--- src/core/schemes/workbook.py
+++ src/core/schemes/workbook.py
@@ -1,16 +1,15 @@
-import os 
+import os
 
-from typing import List, Optional, Union
+from typing import List, Optional, Union, Tuple, Dict
 
 import re
 
 from collections import Counter
 
 from pydantic import BaseModel, root_validator
+from pydantic.error_wrappers import ValidationError
 
 from src.core.loader import SchemaLoader
-
-
 
 
 class Attribute(BaseModel):
@@ -18,18 +17,20 @@ class Attribute(BaseModel):
     Атрибут, включающий в себя указание на конкретную позицию Y-index в столбцах
     """
 
-    name:str 
-    index:int
-    optional:bool
+    name: str
+    index: int
+    optional: bool
+
 
 class Column(BaseModel):
     """ 
     Столбец с указанием признаков для текстового поиска и создания объектов из найденных результатов
-    """  
-    name:str
-    regex:str
-    optional:bool
-    index:Optional[int]
+    """
+    name: str
+    regex: str
+    optional: bool
+    index: Optional[int]
+
 
 class Header(BaseModel):
 
@@ -41,16 +42,70 @@ class Header(BaseModel):
         объединенные ячейки с наименованием документа, датой и т.п.
 
     """
-       
-    columns:List[Column]
-    attributes:Optional[List[Attribute]]
-
+    columns: List[Column] = []
+    attributes: Optional[List[Attribute]]
 
     @root_validator
     def validate_columns(cls, values):
         if not 'columns' in values:
-            raise ValueError('В схеме отстутствует описание столбцов листа Excel')
+            raise ValueError(
+                'В схеме отстутствует описание столбцов листа Excel')
         return values
+
+    def _update_column(self, value: dict):
+        try:
+            result: Column = next(
+                c for c in self.columns if c.name == value['name'])
+        except:
+            raise ValueError
+        else:
+            for k, v in value.items():
+                result.__setattr__(k, v)
+
+    def _update_attribute(self, value: dict) -> None:
+        """_update_attribute 
+
+        Обновление атрибута
+
+        Args:
+            value (dict): входные данные
+
+        Raises:
+            AttributeError: в случае отсутствия массива атрибутов у документа
+            ValueError: в случае отсуствтвия совпадений по имени атрибута
+        """
+        if not self.attributes:
+            raise AttributeError('Отсутствуют атрибуты заголовка')
+        try:
+            result: Attribute = next(
+                a for a in self.attributes if a.name == value['name'])
+        except:
+            raise ValueError
+        else:
+            for k, v in value.items():
+                result.__setattr__(k, v)
+
+    def _delete_column(self, value: dict) -> None:
+
+        try:
+            result: Column = next(
+                c for c in self.columns if c.name == value['name'])
+        except:
+            raise ValueError
+        else:
+            self.columns.remove(result)
+
+    def _add_column(self, value: dict):
+        self.columns.append(Column(**value))
+
+    def _delete_attribute(self, value: dict) -> None:
+        pass
+
+    def _add_attribute(self, value: dict) -> None:
+        if not self.attributes:
+            raise AttributeError("Отсутствуют атрибуы заголовка")
+        self.attributes.append(Attribute(**value))
+
 
 class Workbook(BaseModel):
     """ 
@@ -58,21 +113,21 @@ class Workbook(BaseModel):
 
     """
 
-    name:str
-    header:Header   
+    name: str
+    header: Header
 
     @root_validator
     def validate_header(cls, values):
         if not 'header' in values:
-            raise ValueError('В схеме отстутствует описание заголовка листа Excel')
+            raise ValueError(
+                'В схеме отстутствует описание заголовка листа Excel')
         return values
 
-
-    def update_column(self, value:dict):
+    def update_column(self, value: dict):
         """update_column 
 
         Изменение значений в объекте Column из массива
-        
+
         Args:
             value (dict): [description]
 
@@ -84,15 +139,13 @@ class Workbook(BaseModel):
         """
         # поиск в массиве по name или index
         try:
-            col = next(c for c in self.header.columns if c.name in value.keys() or c.index in value.keys())
-            # изменение значений
-            for k,v in value.items():
-                col.__setattr__(k,v)
+            self.header._update_column(value)
         except StopIteration:
             raise ValueError('Не найдена запись для обновления')
+        else:
+            return self.dict()
 
-
-    def update_attribute(self, value:dict) -> None:
+    def update_attribute(self, value: dict) -> Dict[str, str]:
         """update_attribute 
 
         Args:
@@ -102,63 +155,62 @@ class Workbook(BaseModel):
             ValueError:
 
         Returns:
-            [type]: [description]
+            dict: [description]
         """
         # поиск в массиве по name или index
         try:
-            attr = next(c for c in self.header.columns if c.name == str(value.keys()) or c.index == str(value.keys()))
+            self.header._update_attribute(value)
             # изменение значений
         except StopIteration:
             raise ValueError('Не найдена запись для обновления')
         else:
-            for k,v in value.items():
-                attr.__setattr__(k,v)
+            return self.dict()
 
-    def verify_columns(self, source:tuple):
+    def delete_column(self, )
+
+    def verify_columns(self, source: tuple):
         schema = None
         matched = []
         unmatched = []
         for idx, value in enumerate(source, start=1):
             # поиск по совпадению регулярного значения и исходного заголовка. Возвращает None если не найдено значение
-            match = next((c for c in self.header.columns if re.match(c.regex, value)),None)
+            match = next(
+                (c for c in self.header.columns if re.match(c.regex, value)), None)
             # при найденном значении меняет индекс положения по оси X
             if match:
                 match.index = idx
                 matched.append(match)
-        unmatched.extend([c.name for c in self.header.columns if not c.name in [m.name for m in matched] and not c.optional])
+        unmatched.extend([c.name for c in self.header.columns if not c.name in [
+                         m.name for m in matched] and not c.optional])
         # если количество совпадений соответствует кол-ву аргументов, то считать, что схема найдена
         # в ином случае полагать что, схема документа не описана
         if len(matched) == len(source):
-            schema=self.name
-        self.validate_columns(schema,matched, unmatched)
+            schema = self.name
+        return (schema, matched, unmatched)
 
-    def validate_columns(self, schema:Union[str, None], matched:list, unmatched:list):
+    def validate_columns(self, schema: Union[str, None], matched: list, unmatched: list):
         if schema:
-            duplicates = [item for item, count in Counter([m.name for m in matched]).items() if count > 1]
+            duplicates = [item for item, count in Counter(
+                [m.name for m in matched]).items() if count > 1]
             if duplicates:
-                raise ValueError('Дублирование столбцов, документ не отвечает требованиям схемы')
+                raise ValueError(
+                    'Дублирование столбцов, документ не отвечает требованиям схемы')
             if unmatched:
-                raise ValueError(f'Отсутствуют обязательные столбцы {";".join(unmatched)}')
+                raise ValueError(
+                    f'Отсутствуют обязательные столбцы {";".join(unmatched)}')
         else:
             raise ValueError('Схема документа не найдена')
-        return {'schema':schema, 'matched':matched, 'required':unmatched}
-        
+        return {'schema': schema, 'matched': matched, 'required': unmatched}
+
+
 class WorkbookSchemes(SchemaLoader):
 
     @classmethod
-    def load(cls):        
+    def load(cls):
         path = f'{os.environ["SCHEMES_PATH"]}/documents'
         schemes = []
         for file in os.listdir(path):
-            _,tail = os.path.splitext(file)
+            _, tail = os.path.splitext(file)
             if tail == '.json':
                 schemes.append(Workbook(**super().load(f'{path}/{file}')))
         return schemes
-
-
-
-  
-  
-            
-
-
