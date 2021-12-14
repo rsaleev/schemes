a73efe221f2936fb8d4a38788cc624cb7c638d60
rsaleev <rs.aleev@gmail.com>
2021-12-14 14:35:10 +0300

    debug as environment

--- src/service/wsgi.py
+++ src/service/wsgi.py
@@ -1,9 +1,33 @@
+import os
+
+from pathlib import Path
+
+
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+
 from .routes import schemes, validation
 
-app = FastAPI(debug=True)
+
+
+app = FastAPI(debug=bool(os.environ['FASTAPI_DEBUG']))
 
 app.include_router(schemes.router)
 app.include_router(validation.router)
 
 
+# origins = [
+#     "http://localhost.tiangolo.com",
+#     "https://localhost.tiangolo.com",
+#     "http://localhost",
+#     "http://localhost:8080",
+# ]
+
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=origins,
+#     allow_credentials=False,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
