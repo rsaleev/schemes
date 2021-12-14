72b6762653e5a49ddb6e550c263ec9d3b03f8f9c
rsaleev <rs.aleev@gmail.com>
2021-12-14 14:34:39 +0300

    added prefix

--- src/service/routes/validation.py
+++ src/service/routes/validation.py
@@ -1,41 +1,42 @@
 from typing import Tuple, List
 
-import concurrent.futures
 
 from fastapi.routing import APIRouter
 from fastapi import Query, HTTPException, status
-from starlette.responses import JSONResponse
 
 from src.service.models import scheme
 
-from src.core.schemes import WorkbookSchemes, Workbook
-
-router = APIRouter(tags=['scheme', 'validation'])
+from src.core.schemes import WorkbookSchemes, Workbook, Column
 
 
-@router.get('/validate/{source}', response_model=scheme.SchemeResponse, status_code=status.HTTP_200_OK)
-async def validate_schema(source:scheme.SchemeSource, headers:Tuple[str, ...]=Query(...)):
+router = APIRouter(prefix='/validate', tags=['validation'])
+
+
+@router.get('/{source}',status_code=status.HTTP_200_OK, response_model=scheme.ValidationResponse, response_model_exclude_unset=True)
+async def validate_schema(source:scheme.SchemeDataType, headers:List[str] = Query([])):
     schemes = []
-    if source == scheme.SchemeSource.documents:
-        schemes.extend(WorkbookSchemes.load())
-    elif source == scheme.SchemeSource.database:
+    if source == scheme.SchemeDataType.documents:
+        schemes.extend(WorkbookSchemes.read())
+    elif source == scheme.SchemeDataType.database:
         raise HTTPException(status.HTTP_501_NOT_IMPLEMENTED)
     # проверка загрузки схем
     if not schemes:
-        return scheme.SchemeResponse(error='Схема не найдена')
+        return scheme.ValidationResponse(error='Не удалось загрузить схемы')
     # проверка входных данных на совпадение с моделью заголовка документа
     columns_verifying_results = [s.verify_columns(headers) for s in schemes]
-    result = next(cfr for cfr in columns_verifying_results if cfr[0])
-    if not result:
-        return scheme.SchemeResponse(error='Схема не найдена')
-    # проверка на совпадение по схеме, значение отличное от None
     try:
-        output = result.validate_columns(*result)
-    except Exception as e:
-        return scheme.SchemeResponse(error=str(e))
+        result:Tuple[Workbook, List[Column], List[Column]] = next(cfr for cfr in columns_verifying_results if cfr[0])
+    except StopIteration:
+        return scheme.ValidationResponse(error='Схема не найдена')
     else:
-        return scheme.SchemeResponse(data=output)
-            
+        # проверка на совпадение по схеме, значение отличное от None
+        try:
+            output = result[0].validate_columns(result[0].name, result[1], result[2])
+        except Exception as e:
+            return scheme.ValidationResponse(error=str(e))
+        else:
+            return scheme.ValidationResponse(data=output)
+                
 
 
 
