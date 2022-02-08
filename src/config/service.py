3b08f1fe7b52a0dbd9d5dfe5abce0a7e6a2d7983
rsaleev <rs.aleev@gmail.com>
2022-02-08 13:36:36 +0300

    версия 0.1.1

--- src/config/service.py
+++ src/config/service.py
@@ -10,7 +10,7 @@ class Settings(BaseSettings):
 
     fastapi_app_debug: bool = False
     fastapi_app_title:Optional[str] = 'Схемы документов'
-    fastapi_app_version:Optional[str] = '0.1.0'
+    fastapi_app_version:Optional[str] = '0.1.1'
     fastapi_app_author:Dict[str, str] = {'author':'Rostislav Aleev', 'mail':'rs.aleev@gmail.com'}
     cors_allow_methods: Optional[List[str]] = ["POST", "DELETE", "GET", "PUT"]
     cors_allow_origins: Optional[List[str]] = ["*"]
