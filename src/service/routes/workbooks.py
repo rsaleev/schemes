8d75e46c803b19708b34e6b58938ccd948bd96b9
rsaleev <rs.aleev@gmail.com>
2022-02-14 12:37:38 +0300

    исправлены маршруты

--- src/service/routes/workbooks.py
+++ src/service/routes/workbooks.py
@@ -8,7 +8,7 @@ from fastapi import HTTPException, status, Query
 from src.api.scheme.workbook import *
 from src.api import exceptions
 
-from src.api.scheme import WorkbookSchemes
+from src.api.scheme.workbook import WorkbookSchemes
 from src.service.schema.scheme import *
 from src.service.schema.scheme import SchemeColumnRequest 
 from src.service.schema.validation import *
@@ -16,7 +16,7 @@ from src.service.schema.validation import *
 router = APIRouter(prefix='/documents', tags=['Схемы документов/excel'])
 
 
-@router.get('',
+@router.get('/schemes',
             response_model=List[Workbook],
             response_model_exclude_unset=True,
             description='Получить все известные схемы',
@@ -27,7 +27,7 @@ async def fetch_all():
 
 
 @router.get(
-    '/{schema_name}',
+    '/schemes/{schema_name}',
     response_model=Workbook,
     response_model_exclude_unset=True,
     description='Получить схему в соответствие с типом источника и названием',
@@ -54,7 +54,7 @@ async def fetch_scheme(schema_name: str):
         return wb_scheme_match
 
 
-@router.put('/{schema_name}',
+@router.put('/schemes/{schema_name}',
             description='Обновить элемент в схеме документа по названию',
             status_code=200)
 async def modify_scheme_element(schema_name: str,
@@ -87,7 +87,7 @@ async def modify_scheme_element(schema_name: str,
         return
 
 
-@router.delete('/{schema_name}/{element_type}/{element_name}',
+@router.delete('/schemes/{schema_name}/{element_type}/{element_name}',
                description='Удалить элемент в схеме документа по названию',
                status_code=200,
                response_model=SchemeResponse)
@@ -137,7 +137,7 @@ def delete_scheme_element(element_type: SchemeElementType, schema_name: str,
                     return
 
 
-@router.post('/{scheme_name}',
+@router.post('/schemes/{scheme_name}',
              description='Создать новую схему',
              status_code=201,
              response_model=SchemeResponse)
@@ -151,32 +151,8 @@ def add_new_scheme(scheme_name: str, data: Workbook):
         return
 
 
-def check_headers(headers: list):
-    """
-    Сверка списка заголовков столбцов по всем схемам
 
-    Args:
-        headers (list): наименования столбцов
-
-    Raises:
-        exceptions.SchemeNotLoaded: ошибка загрузки схемы
-        exceptions.SchemeNotFound: схема не найдена после проверки
-
-    Returns:
-        [type]: [description]
-    """
-
-    schemes = WorkbookSchemes.read()
-    if not schemes:
-        raise exceptions.SchemeNotLoaded("Ошибка загрузки данных")
-    result = [s.verify_columns(tuple(headers)) for s in schemes]
-    for r in result:
-        if r[0]:
-            return r[0].validate_columns(r[0].name, r[1], r[2], r[3])
-    raise exceptions.SchemeNotFound("Схема не найдена")
-
-
-@router.get('/validation/documents',
+@router.get('/validate',
             status_code=status.HTTP_200_OK,
             response_model=ValidationResponse,
             response_model_exclude_unset=True)
@@ -184,8 +160,12 @@ async def validate_schema(headers: List[str] = Query(...)):
     if not headers:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
                             detail='Не переданы параметы запроса')
-    try:
-        output = check_headers(headers)
-        return ValidationResponse(data=output)
-    except Exception as e:
-        return ValidationResponse(error=str(e))
+    schemes = WorkbookSchemes.read()
+    if not schemes:
+        return ValidationResponse(error="Ошибка загрузки данных")
+    result = [s.verify_columns(tuple(headers)) for s in schemes]
+    for r in result:
+        if r[0]:
+            return ValidationResponse(data=r[0].validate_columns(r[0].name, r[1], r[2], r[3]))
+        else:
+            return ValidationResponse(error="Схема не найдена")
